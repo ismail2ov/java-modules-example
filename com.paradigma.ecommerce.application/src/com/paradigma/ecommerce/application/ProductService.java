7de3ee51db64e60fe5d9ae887ca7797fe82c6131
Ismail Ismailov <ismail2ov@gmail.com>
2023-05-25 12:32:43 +0200

    Create application module

---
+++ com.paradigma.ecommerce.application/src/com/paradigma/ecommerce/application/ProductService.java
@@ -0,0 +1,9 @@
+package com.paradigma.ecommerce.application;
+
+import com.paradigma.ecommerce.domain.Product;
+import java.util.List;
+
+public interface ProductService {
+
+  List<Product> getAllProducts();
+}
---
+++ com.paradigma.ecommerce.application/src/module-info.java
@@ -0,0 +1,5 @@
+module com.paradigma.ecommerce.application {
+  requires com.paradigma.ecommerce.domain;
+
+  exports com.paradigma.ecommerce.application;
+}
